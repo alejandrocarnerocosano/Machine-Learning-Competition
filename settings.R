d307e7f30ab3f98e581c4b28121d361e230c16f2
AlejandroZielke <alexander.ze2005@gmail.com>
2025-10-13 12:55:56 +0200

    file for global settings (paths, themes, colours, packages)

---
+++ settings.R
@@ -0,0 +1,28 @@
+#########################################
+##-----Setup file for the project-----##
+########################################
+
+# Load packages
+packages <- c(
+  "ggplot2",
+  "tidyr",
+  "dplyr",
+  "psych",
+  "inspectdf",
+  "patchwork"
+)
+
+for (pkg in packages) {
+  if (!require(pkg, character.only = TRUE, quietly = TRUE)) {
+    install.packages(pkg)
+    library(pkg, character.only = TRUE)
+  }
+}
+
+# Data Paths
+path_raw <- "Data/raw"
+path_intermediate <- "Data/intermediate"
+
+# Theme
+theme_set(theme_minimal())
+
